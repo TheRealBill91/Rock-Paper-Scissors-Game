e18c0769eb3a9ad12fd782bba5bd36997c8bd7e8
Billy <bc35786@gmail.com>
2023-01-03 17:33:43 -0600

    Plan out how to create part of rock, paper, scissors game

---
+++ script.js
@@ -0,0 +1,8 @@
+//Take user input (either rock, paper, or scissors) and store in variable
+
+
+//Create array containing rock, paper, and scissors as options for computer
+//create variable that generates a random number between one and three
+//return the hands variable with the randomIndex variable as the index value
+
+console.log(Math.random() * 3);
