34746bdce4a8cf376b829915d48350c8744a8ab7
TheRealBill91 <78881002+TheRealBill91@users.noreply.github.com>
2023-01-04 02:14:34 +0000

    Create getComputerChoice(), a function that randomly generates rock, paper, or scissors

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 //Take user input (either rock, paper, or scissors) and store in variable
 
 
-//Create array containing rock, paper, and scissors as options for computer
+//Create array containing rock, paper, and scissors as options for computer:
 //create variable that generates a random number between one and three
 //return the hands variable with the randomIndex variable as the index value
+//
 
-console.log(Math.random() * 3);
+function getComputerChoice() {
+  const options = ['Rock', 'Paper', 'Scissors']
+  const randomIndex = Math.floor(Math.random() * 3);
+  return options[randomIndex]
+}
+
+console.log(getComputerChoice());
+
+//console.log(Math.floor(Math.random() * 3));
