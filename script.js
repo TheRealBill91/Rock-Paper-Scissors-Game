fde3652fc1da020a3ad275a3f95c2e66704a0dc2
TheRealBill91 <78881002+TheRealBill91@users.noreply.github.com>
2023-01-04 02:24:52 +0000

    Comments explaining getComputerChoice() function

--- script.js
+++ script.js
@@ -1,17 +1,21 @@
-//Take user input (either rock, paper, or scissors) and store in variable
-
-
-//Create array containing rock, paper, and scissors as options for computer:
+//Take user input (either rock, paper, or scissors) and store in /Create array containing rock, paper, and scissors as options for computer:
 //create variable that generates a random number between one and three
 //return the hands variable with the randomIndex variable as the index value
 //
 
+
+
+//function that returns either rock, paper, or scissors, with a random chance for each option.
 function getComputerChoice() {
+  //Create array containing rock, paper, or scissors
   const options = ['Rock', 'Paper', 'Scissors']
+
+  //variable that randomly generates a number between 0 and 2
   const randomIndex = Math.floor(Math.random() * 3);
+  
+  //returns either rock, paper, or scissors randomly by using the randomly generated index number.
   return options[randomIndex]
 }
 
 console.log(getComputerChoice());
 
-//console.log(Math.floor(Math.random() * 3));
